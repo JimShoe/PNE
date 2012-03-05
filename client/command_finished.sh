3a216059b4f684e7ecb9d73ffd718a420fa25f63
jimshoe <jimshoe@0d472227-36fb-480a-852c-69a539c2cf24>
2012-03-05 20:51:44 +0000

    made it a little more general
    git-svn-id: https://frcv.net/svn/jimshoe/programming/php/PNE@7 0d472227-36fb-480a-852c-69a539c2cf24

--- client/command_finished.sh
+++ client/command_finished.sh
@@ -3,6 +3,6 @@
 USER="deathstar"
 PASS="banana"
 msg_type="command"
-msg="command<*>Command Finished<*>$1"
+msg="command<*>$1<*>$2"
 
 wget -q -O - -i - <<< "https://dev.throwthemind.com/m/send.php?user=$USER&pass=$PASS&msg_type=$msg_type&msg=$msg" > /dev/null
